5eb02db51d2fcbb5f809f68d0e67312441327a4d
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-02-04 17:03:25 +0100

    Sorting and other options validation

--- ft_ls.c
+++ ft_ls.c
@@ -9,6 +9,21 @@
  *      > [ ] -t: sort by time, newest first; see --time (time/date format with -l; see TIME_STYLE below)
  * */
 
+/* ft_dirent_comparea - compare in ascending order (alphanum) */
+static inline int ft_dirent_comparea(struct dirent *, struct dirent *);
+
+
+/* ft_dirent_compared - compare in descending order (alphanum) */
+static inline int ft_dirent_compared(struct dirent *, struct dirent *);
+
+
+/* ft_dirent_compareat - compare in ascending order (time) */
+static inline int ft_dirent_compareat(struct dirent *, struct dirent *);
+
+
+/* ft_dirent_comparedt - compare in descending order (time) */
+static inline int ft_dirent_comparedt(struct dirent *, struct dirent *);
+
 int main(int ac, char **av) {
     int exitcode = 0;
 
@@ -54,31 +69,67 @@ main_exit:
 extern char *ft_process_subdirs(const char *path) {
     if (!path) { return (0); }
 
-    /* 1. allocate an array for all directory entries... */
-    size_t dirndx = 0;
-    size_t dircnt = ft_dircnt(path);
-    struct dirent **dirents = calloc(dircnt + 1, sizeof(struct dirent *));
-    if (!dirents) {
+    /* 1. open directory on path 'path->content'... */
+    DIR *dir = opendir(path);
+    if (!dir) {
         return (0);
     }
 
-        /* 2. open directory on path 'path->content'... */
-        DIR *dir = opendir(path);
-        if (!dir) {
-            free(dirents); return (0);
+        /* 2. allocate an array for all directory entries... */
+        size_t dirndx = 0;
+        size_t dircnt = ft_dircnt(path);
+        struct dirent **dirents = calloc(dircnt + 1, sizeof(struct dirent *));
+        if (!dirents) {
+            closedir(dir);
+            return (0);
         }
 
             /* 3. extract every directory entry to dirents... */
             for (struct dirent *dirent = readdir(dir); dirent; dirent = readdir(dir)) {
                 dirents[dirndx++] = dirent;
             }
-
                 /* 4. process directory entries... */
                 char *output = 0;
 
-                /* 4.1. insert every name to output string */
+                /* 4.1. sort entries... */
+                int (*compare)(struct dirent *, struct dirent *) = 0;
+                if (g_opt_reverse) {
+                    if (g_opt_time) {
+                        compare = ft_dirent_comparedt;
+                    }
+                    else {
+                        compare = ft_dirent_compared;
+                    }
+                }
+                else if (g_opt_time) {
+                    compare = ft_dirent_compareat;
+                }
+                else {
+                    compare = ft_dirent_comparea;
+                }
+
+                for (size_t i = 0; i < dircnt - 1; i++) {
+                    for (size_t j = 0; j < dircnt - 1 - i; j++) {
+                        struct dirent *d0 = dirents[j];
+                        struct dirent *d1 = dirents[j + 1];
+
+                        if (compare(d0, d1)) {
+                            dirents[j] = d1;
+                            dirents[j + 1] = d0;
+                        }
+                    }
+                }
+
+                /* 4.2. insert every name to output string */
                 for (size_t i = 0; i < dircnt; i++) {
                     struct dirent dirent = *dirents[i];
+                            
+                    /* validate '-a' flag... */
+                    if (*dirent.d_name == '.') {
+                        if (!g_opt_all) {
+                            continue;
+                        }
+                    }
                     
                     output = output == 0 ?
                         ft_strdup(dirent.d_name) :
@@ -87,43 +138,55 @@ extern char *ft_process_subdirs(const char *path) {
                     output = ft_strjoin_free(output, "\n");
                 }
 
-                /* 4.2. process subdirectories recursively... */
-                for (size_t i = 0; i < dircnt; i++) {
-                    struct dirent dirent = *dirents[i];
+                /* 4.3. process subdirectories recursively... */
 
-                    /* avoid "cwd" and "pwd" dirents... */
-                    if (!ft_strcmp(dirent.d_name, ".") ||
-                        !ft_strcmp(dirent.d_name, "..")
-                    ) {
-                        continue;
-                    }
+                /* validate '-R' flag... */
+                if (g_opt_recursive) {
+                    for (size_t i = 0; i < dircnt; i++) {
+                        struct dirent dirent = *dirents[i];
 
-                    if (dirent.d_type == DT_DIR) {
-                        /* create sub-path... */
-                        char *subpath = 0;
-
-                        /* ...also, check if supplied path ends with '/'; if not, append it first... */
-                        if (!ft_strendswith(path, '/')) {
-                            subpath = ft_strjoin(path, "/");
-                            subpath = ft_strjoin_free(subpath, dirent.d_name);
-                        }
-                        else {
-                            subpath = ft_strjoin(path, dirent.d_name);
-                        }
-
-                        char *tmp0 = ft_process_subdirs(subpath);
-                        if (!tmp0) {
-                            free(subpath), subpath = 0;
+                        /* avoid "cwd" and "pwd" dirents... */
+                        if (!ft_strcmp(dirent.d_name, ".") ||
+                            !ft_strcmp(dirent.d_name, "..")
+                        ) {
                             continue;
                         }
 
-                        output = ft_strjoin_free(output, "\n");
-                        output = ft_strjoin_free(output, subpath);
-                        output = ft_strjoin_free(output, ":\n");
-                        output = ft_strjoin_free(output, tmp0);
-                        
-                        free(subpath), subpath = 0;
-                        free(tmp0), tmp0 = 0;
+                        if (dirent.d_type == DT_DIR) {
+
+                            /* validate '-a' flag... */
+                            if (*dirent.d_name == '.') {
+                                if (!g_opt_all) {
+                                    continue;
+                                }
+                            }
+
+                            /* create sub-path... */
+                            char *subpath = 0;
+
+                            /* ...also, check if supplied path ends with '/'; if not, append it first... */
+                            if (!ft_strendswith(path, '/')) {
+                                subpath = ft_strjoin(path, "/");
+                                subpath = ft_strjoin_free(subpath, dirent.d_name);
+                            }
+                            else {
+                                subpath = ft_strjoin(path, dirent.d_name);
+                            }
+
+                            char *tmp0 = ft_process_subdirs(subpath);
+                            if (!tmp0) {
+                                free(subpath), subpath = 0;
+                                continue;
+                            }
+
+                            output = ft_strjoin_free(output, "\n");
+                            output = ft_strjoin_free(output, subpath);
+                            output = ft_strjoin_free(output, ":\n");
+                            output = ft_strjoin_free(output, tmp0);
+                            
+                            free(subpath), subpath = 0;
+                            free(tmp0), tmp0 = 0;
+                        }
                     }
                 }
 
@@ -132,6 +195,88 @@ extern char *ft_process_subdirs(const char *path) {
     free(dirents);
 
     return (output);
+}
+
+
+/* ft_dirent_comparea - compare in ascending order (alphanum) */
+static inline int ft_dirent_comparea(struct dirent *d0, struct dirent *d1) {
+    const char *n0 = d0->d_name;
+    const char *n1 = d1->d_name;
+    
+    /* special case: n0 == "..", n1 == "." */
+    if (!ft_strcmp(n0, "..") &&
+        !ft_strcmp(n1, ".")
+    ) {
+        return (1);
+    }
+
+    while (*n0 || *n1) {
+        while (*n0 && !ft_isalnum(*n0)) { n0++; }
+        while (*n1 && !ft_isalnum(*n1)) { n1++; }
+        if (ft_tolower(*n0) != ft_tolower(*n1)) {
+            return (ft_tolower(*n0) > ft_tolower(*n1));
+        }
+
+        if (*n0) { n0++; }
+        if (*n1) { n1++; }
+    }
+
+    return (ft_strcmp(n0, n1) < 0);
+}
+
+
+/* ft_dirent_compared - compare in descending order (alphanum) */
+static inline int ft_dirent_compared(struct dirent *d0, struct dirent *d1) {
+    const char *n0 = d0->d_name;
+    const char *n1 = d1->d_name;
+
+    /* special case: n0 == ".", n1 == ".." */
+    if (!ft_strcmp(n0, ".") &&
+        !ft_strcmp(n1, "..")
+    ) {
+        return (1);
+    }
+
+    while (*n0 || *n1) {
+        while (*n0 && !ft_isalnum(*n0)) { n0++; }
+        while (*n1 && !ft_isalnum(*n1)) { n1++; }
+        if (ft_tolower(*n0) != ft_tolower(*n1)) {
+            return (ft_tolower(*n0) < ft_tolower(*n1));
+        }
+
+        if (*n0) { n0++; }
+        if (*n1) { n1++; }
+    }
+
+    return (ft_strcmp(n0, n1) > 0);
+}
+
+
+/* ft_dirent_compareat - compare in ascending order (time) */
+static inline int ft_dirent_compareat(struct dirent *d0, struct dirent *d1) {
+    struct stat st = { 0 };
+    
+    if (stat(d0->d_name, &st) == -1) { return (0); }
+    size_t t0 = st.st_mtime;
+    
+    if (stat(d1->d_name, &st) == -1) { return (0); }
+    size_t t1 = st.st_mtime;
+
+    return (t0 >= t1);
+}
+
+
+/* ft_dirent_comparedt - compare in descending order (time) */
+static inline int ft_dirent_comparedt(struct dirent *d0, struct dirent *d1) {
+    struct stat st = { 0 };
+    
+    if (stat(d0->d_name, &st) == -1) { return (0); }
+    size_t t0 = st.st_mtime;
+    
+    if (stat(d1->d_name, &st) == -1) { return (0); }
+    size_t t1 = st.st_mtime;
+
+    return (t0 <= t1);
 }
 
 
